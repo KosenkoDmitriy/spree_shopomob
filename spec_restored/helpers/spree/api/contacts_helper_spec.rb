dae3ff8269e41c61363532cc23fcf07e34fb8ab7
dmitry <kosenkodmitryv@gmail.com>
2014-10-19 22:36:19 +0400

    restored spec folder

---
+++ spec_restored/helpers/spree/api/contacts_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Spree::Api::ContactsHelper. For example:
+#
+# describe Spree::Api::ContactsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+describe Spree::Api::ContactsHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
