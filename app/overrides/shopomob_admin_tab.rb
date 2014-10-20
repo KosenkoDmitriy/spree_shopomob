f269ac27a7fd145c974540570c11742fec1d5699
dmitry <kosenkodmitryv@gmail.com>
2014-10-20 20:43:58 +0400

    update ru translations. fix error when edit any order.

--- app/overrides/shopomob_admin_tab.rb
+++ app/overrides/shopomob_admin_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "shopomob_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab :about, :label => 'Shopomob', :url => '/admin/about', :icon => 'icon-shopping-cart' %>",
+                     :text => "<%= tab :about, :label => Spree.t('sm.shopomob'), :url => '/admin/about', :icon => 'icon-shopping-cart' %>",
                      :disabled => false)
