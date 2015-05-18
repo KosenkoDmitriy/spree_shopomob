b9871f1eee7d744e0298de45e4e6c0f68a3ea627
dmitry <kosenkodmitryv@gmail.com>
2015-05-18 23:42:53 +0300

    support pagination for news

--- app/controllers/spree/api/news_controller.rb
+++ app/controllers/spree/api/news_controller.rb
@@ -1,6 +1,12 @@
 class Spree::Api::NewsController < Spree::Api::BaseController
   def index
-    @news = Spree::News.order(:updated_at)#.reverse
+    limit1 = params[:page].to_i
+    limit2 = params[:per_page].to_i
+    if (limit1 > 0)
+      limit1 = limit1*limit2
+    end
+    @news = Spree::News.order(:updated_at).offset(limit1).limit(limit2)#.reverse
+    @total_count = Spree::News.count
 #    respond_with(@news)
   end
   
--- app/views/spree/api/news/index.v1.rabl
+++ app/views/spree/api/news/index.v1.rabl
@@ -3,5 +3,7 @@ child(@news => :news) do
  extends "spree/api/news/show"
 end
 
+node(:total_count) { @total_count }
 node(:count) { @news.count }
 node(:current_page) { params[:page] || 1 }
+
