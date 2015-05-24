28adfcaa7aa952c692d52e0a356dce68dac615a3
dmitry <kosenkodmitryv@gmail.com>
2015-05-24 18:35:12 +0300

    fixed pagination for cats/products

--- app/controllers/spree/api/categories_controller.rb
+++ app/controllers/spree/api/categories_controller.rb
@@ -3,11 +3,8 @@ class Spree::Api::CategoriesController < Spree::Api::BaseController
   def index
     id = params[:id].to_i
     per_page = params[:per_page].to_i
-    if (per_page <= 0)
-      @taxons = Spree::Taxon.order(:taxonomy_id, :lft)
-      respond_with(@taxons)
-      return
-    end
+    page = params[:page].to_i
+
 
     if (id > 0)
       if (Spree::Taxonomy.exists?(id))
@@ -18,11 +15,23 @@ class Spree::Api::CategoriesController < Spree::Api::BaseController
       else
         @taxons = Spree::Taxon.where(taxonomy_id: id, parent_id: id)#.order(:name, :lft) #commented order because {"exception":"SQLite3::SQLException: no such column: spree_taxon_translations.name: SELECT \"spree_taxons\".* FROM \"spree_taxons\" WHERE \"spree_taxons\".\"taxonomy_id\" = ? AND \"spree_taxons\".\"parent_id\" = ?  ORDER BY spree_taxon_translations.name"}
       end
-    elsif (id == 0)
+    elsif (id == 0) #parent cats/subcats
       @taxons = Spree::Taxon.where(parent_id:nil)
     else
       @taxons = Spree::Taxon.order(:taxonomy_id, :lft)
     end
+
+
+    if (page > 0 && per_page > 0)
+      page = page * per_page
+      @taxons = @taxons.offset(page).limit(per_page)
+      #respond_with(@taxons)
+      #return
+    end
+
+    #respond_with(@taxons)
+    #return
+
 
 
     #if (@taxons.present?)
@@ -31,8 +40,6 @@ class Spree::Api::CategoriesController < Spree::Api::BaseController
     #  format.json { render :json => @taxons }
     #end
 
-    respond_with(@taxons)
-    return
     #respond_to do |format|
     #  format.json { render :json => {} }
     #end
@@ -48,20 +55,31 @@ class Spree::Api::CategoriesController < Spree::Api::BaseController
     limit1 = params[:page].to_i
     limit2 = params[:per_page].to_i
     per_page = params[:per_page].to_i
-
-    @total_count = Spree::Product.count
-    if (limit2 <= 0)
-      @products = Spree::Product.order(:updated_at)
-      respond_with(@products)
-      return
+    if (limit1 > 0 && limit2 > 0)
+      limit1 = limit1 * limit2
     end
+
+    #  @products = Spree::Product.order(:updated_at)
+    #  respond_with(@products)
+    #  return
+    #end
 
     if (id > 0)
       @taxon = Spree::Taxon.find(params[:id])
-      @products = @taxon.products.offset(limit1).limit(limit2)
-
+      @total_count = @taxon.products.count
+      if (limit2 <= 0) #display all products
+        @products = @taxon.products
+      else
+        @products = @taxon.products.offset(limit1).limit(limit2)
+      end
     else
-      @products = Spree::Product.order(:updated_at).offset(limit1).limit(limit2)
+      if (limit2 <= 0) #display all products
+        @products = Spree::Product.order(:updated_at)
+        @total_count = @products.count
+      else
+        @products = Spree::Product.order(:updated_at).offset(limit1).limit(limit2)
+        @total_count = Spree::Product.count
+      end
     end
 
     #@product = Spree::Product.first
